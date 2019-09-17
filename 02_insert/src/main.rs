54cbf1a9fdf9b9ea33dc298b8a2989cd4092e694
Sean Griffin <sean@seantheprogrammer.com>
2019-09-17 12:01:40 -0600

    Insert exercise

---
+++ 02_insert/src/main.rs
@@ -0,0 +1,79 @@
+#[macro_use]
+extern crate diesel;
+
+mod schema;
+
+use diesel::prelude::*;
+use failure::Fallible;
+
+use crate::schema::*;
+
+#[derive(PartialEq, Debug)]
+#[derive(Queryable)]
+struct Post {
+    id: i32,
+    title: String,
+    body: Option<String>,
+}
+
+impl Post {
+    fn new(id: i32, title: &str, body: Option<&str>) -> Self {
+        Self {
+            id,
+            title: title.into(),
+            body: body.map(Into::into),
+        }
+    }
+}
+
+struct NewPost<'a> {
+    title: &'a str,
+    body: &'a str,
+}
+
+fn save_new_post(conn: &PgConnection, title: &str, body: &str) -> QueryResult<()> {
+    Ok(())
+}
+
+fn save_new_posts(conn: &PgConnection, new_posts: Vec<NewPost>) -> QueryResult<Vec<Post>> {
+    Ok(Vec::new())
+}
+
+#[test]
+fn inserting_single_record() -> Fallible<()> {
+    let conn = connection()?;
+
+    save_new_post(&conn, "Hello", "This is a post")?;
+    let expected_posts = vec![
+        Post::new(1, "Hello", Some("This is a post")),
+    ];
+    assert_eq!(expected_posts, posts::table.load(&conn)?);
+
+    Ok(())
+}
+
+#[test]
+fn inserting_many_records() -> Fallible<()> {
+    let conn = connection()?;
+
+    let posts = save_new_posts(&conn, vec![
+        NewPost { title: "First Post", body: "Please Read" },
+        NewPost { title: "Second Post", body: "Please Ignore" },
+    ])?;
+    let expected_posts = vec![
+        Post::new(1, "First Post", Some("Please Read")),
+        Post::new(2, "Second Post", Some("Please Ignore")),
+    ];
+    assert_eq!(expected_posts, posts);
+
+    Ok(())
+}
+
+fn connection() -> Fallible<PgConnection> {
+    let database_url = dotenv::var("DATABASE_URL")?;
+    let connection = PgConnection::establish(&database_url)?;
+    connection.begin_test_transaction()?;
+    diesel::sql_query("LOCK TABLE posts").execute(&connection)?;
+    diesel::sql_query("SELECT setval('posts_id_seq', 1, false)").execute(&connection)?;
+    Ok(connection)
+}
